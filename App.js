c46d328cae210204aa98a2edd5e16679e4ecf529
Gabriel <gab.atienza@gmail.com>
2020-08-12 14:33:34 -0700

    listitem

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from 'components/Header';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import ListItem from 'components/ListItem';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {v4 as uuid} from 'uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} />}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    height: 100,
     flex: 1,
     paddingTop: 60,
-    // justifyContent: 'center',
-    // alignItems: 'center',
   },
 });
 
